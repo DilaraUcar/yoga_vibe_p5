d90ec82e6c34138fb8333708ab801b3f00e3b1e3
Dilara Ucar <dilara.ucaaar@gmail.com>
2024-05-11 20:41:04 +0000

    Added product app, models and updated admin

---
+++ products/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    """
+    Represents a category for grouping products.
+    """
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """
+    Represents a product available for sale.
+    """
+
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    stock_quantity = models.PositiveIntegerField(default=0)
+    rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(default="product_images/noimage.png", null=True, blank=True)
+
+    def __str__(self):
+        return self.name
