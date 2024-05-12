2eae24c4bbf4ae5e605dbf402e7541157a4d5d1c
Dilara Ucar <dilara.ucaaar@gmail.com>
2024-05-12 01:15:59 +0000

    Installed summernote and customized admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,20 +1,21 @@
 from django.contrib import admin
 from .models import Product, Category
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
-
-class ProductAdmin(admin.ModelAdmin):
+@admin.register(Product)
+class ProductAdmin(SummernoteModelAdmin):
     list_display = (
-        'sku',
         'name',
+        'sku',
         'category',
         'price',
         'rating',
         'image',
         'description',
     )
-
+    search_fields = ['name']  # Change 'title' to 'name' if 'name' is the field you want to search
     ordering = ('sku',)
+    summernote_fields = ('description',)  # Use 'description' here
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
@@ -22,5 +23,4 @@ class CategoryAdmin(admin.ModelAdmin):
         'name',
     )
 
-admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
