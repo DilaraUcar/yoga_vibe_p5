326070897f978e8849f9ddea4661f4ff44073e89
Dilara Ucar <dilara.ucaaar@gmail.com>
2024-05-26 09:30:26 +0000

    Deployment settings

--- yoga_vibe/settings.py
+++ yoga_vibe/settings.py
@@ -132,16 +132,16 @@ WSGI_APPLICATION = 'yoga_vibe.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
- # DATABASES = {
- #     'default': {
- #         'ENGINE': 'django.db.backends.sqlite3',
- #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
- #     }
- # }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+     }
 
 
 # Password validation
