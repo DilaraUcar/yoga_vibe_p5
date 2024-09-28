42006b57788ccb84baf6e987a335da250d6687d3
Dilara Ucar <dilara.ucaaar@gmail.com>
2024-09-28 13:08:51 +0000

    Debug setting false for deployment

--- yoga_vibe/settings.py
+++ yoga_vibe/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'yoga-vibe-pp5-63b402220300.herokuapp.com', 'localhost',
