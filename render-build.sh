cb228f1239dd40246b35d7b1b3096ab74c28d008
DilaraUcar <dilara.ucaaar@gmail.com>
2026-01-12 22:45:00 +0000

    Update render-build.sh for deployemnt error

--- render-build.sh
+++ render-build.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
-
-# Exit if anything fails
-set -e
-
-# Update OS packages
-apt-get update
-
-# Install libraries needed for Pillow
-apt-get install -y libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev \
-                   libtiff5-dev tcl8.6-dev tk8.6-dev python3-tk
+set -e  # Exit if anything fails
 
 # Upgrade pip
 pip install --upgrade pip
