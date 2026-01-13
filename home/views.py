713ef36ec570228ce4423f3f33332edd6c8800d4
DilaraUcar <dilara.ucaaar@gmail.com>
2026-01-13 20:22:02 +0000

    Temporary admin creation route for Render

--- home/views.py
+++ home/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def create_render_admin(request):
+    username = "admin"
+    email = "dilaraucar.t@gmail.com"
+    password = "StrongPassword123!"
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
+        return HttpResponse("✅ Superuser created. REMOVE THIS VIEW NOW.")
+    else:
+        return HttpResponse("⚠️ Superuser already exists. REMOVE THIS VIEW NOW.")
