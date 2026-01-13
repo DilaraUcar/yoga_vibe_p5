16052c7bb3516c1a8646015efc48e8bcb8975d75
DilaraUcar <dilara.ucaaar@gmail.com>
2026-01-13 21:12:16 +0000

    Add temorary admin view for render

--- home/views.py
+++ home/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
 
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
 
+
+def create_admin(request):
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@example.com",
+            password="YourSecurePassword123!"
+        )
+        return HttpResponse("Superuser created!")
+    return HttpResponse("Superuser already exists!")
